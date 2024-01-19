4dbe9492596799fa06d4a98d738833fb7aa7f5cd
zun669 <98329205+zun669@users.noreply.github.com>
2024-01-19 15:03:37 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,55 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int a = rand.Next(1, 24);           
+            int a = rand.Next(1, 24);
+            int b = rand.Next(1, 24);
+            int c = rand.Next(1, 24);
             int count = 1;
             int Score = 0;
-            Console.WriteLine("Компьютер загадал три чесла, попробуй отгадать, у Вас три попытки");
+            Console.WriteLine("Компьютер загадал три числа, попробуйте отгадать их");
             Console.WriteLine("Введите число:");
             int k = Convert.ToInt32(Console.ReadLine());
 
-            while (count <=3)
-            
+
+
+
+            while (count <= 3)
             {
-                if ( a == k)
+                if (a == k || b == k || c == k)
                 {
+                    count = 1;
                     Score++;
                     Console.WriteLine("Верно! Счётчик баллов: " + Score + "");
-                    
-                        break;
+                    Console.WriteLine("Компьютер снова загадал три чесла, попробуй отгадать, у Вас три попытки");
+                    Console.WriteLine("Введите число:");
+                    a = rand.Next(1, 24);
+                    b = rand.Next(1, 24);
+                    c = rand.Next(1, 24);
+                    k = Convert.ToInt32(Console.ReadLine());
+
                 }
                 else
                 {
                     count++;
                     if (count == 4)
                     {
-                        Console.WriteLine("Попытки закончились");
-                        break;
-                    }
-                    Console.WriteLine("Нет, это не " + k + ", это " + a + "!Попытка № " + count + ":");
+                    
+                        Console.WriteLine("Попытки закончились, Ваш счёт "+Score+ "");
+                        break;                      
+                    }  
+                    
+                    Console.WriteLine("Нет, это не " + k + ",  это были числа: " + a + ", " + b + ", " + c + " Попытка № " + count + ":");
                     k = Convert.ToInt32(Console.ReadLine());
                 }
             }
             Console.ReadLine();
+            
 
         }
     }
